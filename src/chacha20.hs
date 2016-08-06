b5701c88cd8fe42e6b809ee4d99ea1f217d59022
Christopher Wood <christopherwood07@gmail.com>
2016-08-06 12:40:34 -0700

    Updates

--- src/chacha20.hs
+++ src/chacha20.hs
@@ -6,6 +6,12 @@ import Numeric (showHex, readHex, showIntAtBase)
 toWord32 :: [Char] -> Word32
 toWord32 string = fromIntegral (fst ((readHex string) !! 0))
 
+printHex :: Word32 -> IO()
+printHex x = putStrLn $ showHex x ""
+
+flipWord :: Word32 -> Word32
+flipWord x = (shiftL (x .&. 0xFF) 24) .|. (shiftL (x .&. 0xFF00) 8) .|. (shiftR (x .&. 0xFF0000) 8) .|. (shiftR (x .&. 0xFF000000) 24)
+
 quarterRound1 :: (Word32, Word32, Word32, Word32) -> (Word32, Word32, Word32, Word32)
 quarterRound1 (a, b, c, d) = do
     let aprime = a + b
@@ -50,6 +56,23 @@ testFullQuarterRound = do
 
 type ChaChaState = [Word32]
 
+displayState :: ChaChaState -> IO()
+displayState state = mapM_ printHex state
+
+flipState :: ChaChaState -> ChaChaState
+flipState state = map flipWord state
+
+word32ToBytes :: Word32 -> [Word8]
+word32ToBytes x = map fromIntegral [(shiftR (x .&. 0xFF000000) 24), (shiftR (x .&. 0xFF0000) 16), (shiftR (x .&. 0xFF00) 8), ((x .&. 0xFF))]
+
+merge :: [[Word8]] -> [Word8]
+merge [] = []
+merge ([]:xss) = merge xss
+merge ((x:xs):y) = [x] ++ xs ++ (merge y)
+
+stateToBytes :: ChaChaState -> [Word8] 
+stateToBytes state = merge (map word32ToBytes state)
+
 replaceNthElement :: Int -> Word32 -> [Word32] -> [Word32]
 replaceNthElement n v (x:xs)
     | n == 0 = v:xs
@@ -68,5 +91,63 @@ testState = do
                  toWord32 "5c971061", toWord32 "3d631689", toWord32 "2098d9d6", toWord32 "91dbd320"]
     print state
     let newState = quarterRound state (2, 7, 8, 13)
-        in print newState
+        in displayState newState
+
+chaCha20BlockRound :: ChaChaState -> ChaChaState
+chaCha20BlockRound state = do
+    let state1 = quarterRound state  (0, 4,  8, 12)
+    let state2 = quarterRound state1 (1, 5,  9, 13)
+    let state3 = quarterRound state2 (2, 6, 10, 14) 
+    let state4 = quarterRound state3 (3, 7, 11, 15)
+    let state5 = quarterRound state4 (0, 5, 10, 15)
+    let state6 = quarterRound state5 (1, 6, 11, 12)
+    let state7 = quarterRound state6 (2, 7,  8, 13)
+    let state8 = quarterRound state7 (3, 4,  9, 14)
+        in 
+            state8
+
+chaCha20BlockLoop :: ChaChaState -> Int -> ChaChaState
+chaCha20BlockLoop state n
+    | n == 0 = state
+    | otherwise = chaCha20BlockLoop (chaCha20BlockRound state) (n - 1)
     
+-- Main block function
+chaCha20Block :: [Word32] -> [Word32] -> Word32 -> [Word32]
+chaCha20Block key nonce count 
+    | length key /= 8 = error "Invalid key length -- must be 256 bits"
+    | length nonce /= 3 = error "Invalid nonce length -- must be 96 bits"
+    | otherwise = do
+        let state = [toWord32 "61707865", toWord32 "3320646e", toWord32 "79622d32", toWord32 "6b206574",
+                     flipWord (key !! 0), flipWord (key !! 1), flipWord (key !! 2), flipWord (key !! 3),
+                     flipWord (key !! 4), flipWord (key !! 5), flipWord (key !! 6), flipWord (key !! 7),
+                     count, flipWord (nonce !! 0), flipWord (nonce !! 1), flipWord (nonce !! 2)]
+        
+        let mixedState = chaCha20BlockLoop state 10
+            in
+                flipState (zipWith (+) state mixedState)
+
+testChaCha20Block :: IO()
+testChaCha20Block = do
+    let key = [toWord32 "00010203", toWord32 "04050607", toWord32 "08090a0b", toWord32 "0c0d0e0f", 
+               toWord32 "10111213", toWord32 "14151617", toWord32 "18191a1b", toWord32 "1c1d1e1f"]
+    let nonce = [toWord32 "00000009", toWord32 "0000004a", toWord32 "00000000"]
+    let counter = toWord32 "1"
+    let state = chaCha20Block key nonce counter
+        in
+            displayState state
+
+chaCha20EncryptBlock :: [Word32] -> Word32 -> [Word32] -> [Word8] -> [Word8]
+chaCha20EncryptBlock key counter nonce block = do
+    let keyStream = chaCha20Block key nonce counter 
+    let pad = stateToBytes keyStream
+        in
+            zipWith xor pad block
+
+chaCha20Encrypt :: [Word32] -> Word32 -> [Word32] -> [Word8] -> [Word8]
+chaCha20Encrypt key counter nonce plaintext = do
+    let upperRange = (length plaintext) `div` 8
+        in 
+            error "TODO"
+
+
+
